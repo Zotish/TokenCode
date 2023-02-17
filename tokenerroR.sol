15142e7eb9c2ff7c2ddc756f05cd126304bc17a9
Zotish Chandra <72625524+Zotish@users.noreply.github.com>
2023-02-17 19:19:10 +0600

    Update tokenerroR.sol

--- tokenerroR.sol
+++ tokenerroR.sol
@@ -1,103 +1,81 @@
-
+pragma solidity 0.8.0;
 // SPDX-License-Identifier: MIT
 
-
-pragma solidity ^0.8.0;
-abstract contract Implementation{
+abstract contract Ierc20{
 function name() public virtual view returns (string memory);
 function symbol() public virtual view returns (string memory);
-function decimals() public virtual view returns (uint8);
-function totalSupply() public virtual view returns (uint256);
+function decimals() public  virtual view returns (uint256);
+function totalSupply() public view virtual returns (uint256);
 function balanceOf(address _owner) public virtual view returns (uint256 );
-function transfer(address _to, uint256 _value) public virtual returns (bool );
-function transferFrom(address _from, address _to, uint256 _value) public override returns (bool );
-function approve(address _spender, uint256 _value) public  virtual returns (bool );
-function allowance(address _owner, address _spender) public  virtual view returns (uint256 );
+function transfer(address _to, uint256 _value) public virtual returns (bool);
+function transferFrom(address _from, address _to, uint256 _value) public virtual returns (bool);
+function approve(address _spender, uint256 _value) public virtual returns (bool );
+function allowance(address _owner, address _spender) public view virtual  returns (uint256 remaining);
 event Transfer(address indexed _from, address indexed _to, uint256 _value);
 event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
 
 }
-contract ownerstatus{
-     address owner;
-     address newowner;
-
-  constructor (){
-      owner ==msg.sender;
-                }
-     
-     function ownership_change(address _to) public { 
-         require(msg.sender==owner,"You are not the owner ");
-         newowner=_to;
-        }
-        function accept_ownership()public   returns(address){
-            require( msg.sender==newowner," Only new owner can excute the function");
-            owner=newowner;
-            return newowner;
-        }
-    }
-     
+ contract erc20 is Ierc20{
+     address internal owner;
+     string internal tokenName;
+     string internal tokenSymbol;
+     uint256 internal Decimals;
+     uint256 internal TotalSupply;
+     mapping (address=>uint256) public checkBalance;
+     mapping (address=>mapping(address=>uint256)) public GetBalance;
 
 
-contract mytoken is Implementation,ownerstatus {
-
-    string  public _token_name;
-    string  public _symbol;
-    uint8 public  _token_decimal;
-    uint256 public _total_supply;
-    mapping(address=>uint) balances;
-    mapping(address=>mapping(address=>uint256)) approval;
+     constructor (){
+         owner=msg.sender;
+         tokenName="test Token ";
+         tokenSymbol="TSTE";
+         Decimals=4;
+         TotalSupply=10000000000;
+         checkBalance[msg.sender]=TotalSupply;
+     }
 
 
 
-    constructor(){
-        _token_name="Zotish";
-        _symbol="ztc";
-        _token_decimal=8;
-        _total_supply=1000000000000;
-        balances[msg.sender]=_total_supply; 
-
-    }
-function name() public override view  returns (string memory ){
-        return _token_name;
+function name() public view override returns (string memory){
+         return tokenName;
 }
-function symbol() public override  view returns (string memory ){
-        return _symbol;
+function symbol() public view  override returns (string memory){
+     return tokenSymbol;
 }
-function decimals() public override view  returns (uint8){
-        return _token_decimal;
+function decimals() public view  override returns (uint256){
+         return Decimals;
 }
-function totalSupply() public override view  returns (uint256){
-        return _total_supply;
+function totalSupply() public view  override  returns (uint256){
+         return TotalSupply;
 }
-function balanceOf(address _owner) public override view   returns (uint256 ){
-        return balances[_owner];
+function balanceOf(address _owner) public override view returns (uint256 ){
+          return checkBalance[_owner];
 }
-function transfer(address _to, uint256 _value) public override  returns (bool ){
-         require(balances[msg.sender]>=_value,"you donot have sufficient balance ");
-         balances[msg.sender] -= _value;
-         balances[_to] +=_value;
-        emit Transfer(msg.sender,_to,_value);
-        return true;
+function transfer(address _to, uint256 _value) public override returns (bool){
+                      require(checkBalance[msg.sender]>=0);
+                      checkBalance[msg.sender]-=_value;
+                      checkBalance[_to]+=_value;
+                      emit Transfer(msg.sender,_to,_value);
+                      return true;
 }
-function transferFrom(address _from, address _to, uint256 _value) public override returns (bool ){
-           uint256 allowbal =approval[_from][msg.sender]=_value;
-           require(allowbal>=_value,"insu");
-           return true;
-        
+function transferFrom(address _from, address _to, uint256 _value) public override returns (bool){
+                   checkBalance[_from]-=_value;
+                   GetBalance[_from][msg.sender]-=_value;
+                   checkBalance[_to]+=_value;
+                   emit Transfer(_from,_to,_value);
+                   return true;
+                   
 }
-function approve(address _spender, uint256 _value) public  override returns (bool ){
-           require(balances[msg.sender] >=_value, "Inusuffiecnet balance ");
-         approval[msg.sender][_spender]=_value;
-         emit Approval(msg.sender,_spender,_value);
-         return true;
-         
+function approve(address _spender, uint256 _value) public override returns (bool ){
+                         require(checkBalance[msg.sender]>=_value);
+                         require(_spender!=address(0));
+                         GetBalance[msg.sender][_spender]=_value;
+                         emit Approval(msg.sender,_spender,_value);
+                         return true;
 }
-function allowance(address _owner, address _spender) public  override view  returns (uint256 ){
-              return approval[_owner][_spender];
+function allowance(address _owner, address _spender) public view override returns (uint256 ){
+                        return  GetBalance[_owner][_spender];
 }
-
-
-
 
 }
